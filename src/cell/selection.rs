ecd8dfc34c6dcc2920cecc426abf5237b3f366a9
Skylor R Schermer <skyschermer@gmail.com>
2020-07-01 12:53:13 -0500

    Tolerate whitespace in CellSelection parser.

--- src/cell/selection.rs
+++ src/cell/selection.rs
@@ -12,6 +12,9 @@
 use crate::cell::CellSelector;
 use crate::basic::BasicPalette;
 use crate::parse::cell_selection;
+use crate::parse::circumfix;
+use crate::parse::whitespace;
+use crate::parse::maybe;
 use crate::parse::ParseResultExt as _;
 use crate::error::Error;
 
@@ -40,7 +43,10 @@ pub struct CellSelection<'name>(Vec<CellSelector<'name>>);
 impl<'name> CellSelection<'name> {
     /// Parses a CellSelection from the given string.
     pub fn parse(input: &'name str) -> Result<Self, Error> {
-        cell_selection(input)
+        circumfix(
+            cell_selection,
+            maybe(whitespace))
+        (input)
             .expect_end_of_text()
             .map_err(Error::from)
             .map(|success| success.value)
