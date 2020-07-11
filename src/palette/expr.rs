fbd65ce4a889771d09b4d384a85ee1d67f07087f
Skylor R Schermer <skyschermer@gmail.com>
2020-07-11 18:36:30 -0500

    Implement cubic interpolation.

--- src/palette/expr.rs
+++ src/palette/expr.rs
@@ -24,10 +24,70 @@ use serde::Serialize;
 use std::collections::HashSet;
 
 ////////////////////////////////////////////////////////////////////////////////
+// Interpolate
+////////////////////////////////////////////////////////////////////////////////
+/// Color interpolation function for ramps.
+#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Serialize, Deserialize)]
+pub enum Interpolate {
+    /// Linear interpolation over each RGB channel.
+    LinearRgb {
+        /// The interpolation factor.
+        amount: f32,
+    },
+    
+    /// Cubic interpolation over each RGB channel.
+    CubicRgb {
+        /// The slope of the start color.
+        start_slope: f32,
+        /// The slope of the end color.
+        end_slope: f32,
+        /// The interpolation factor.
+        amount: f32,
+    },
+}
+
+impl Interpolate {
+    /// Applies the interpolation function to the given colors.
+    fn apply<C, D>(&self, start: C, end: D) -> Color 
+        where
+            C: Into<Color> + Sized,
+            D: Into<Color> + Sized,
+    {
+        use Interpolate::*;
+        match self {
+            LinearRgb { amount } => {
+                Color::rgb_linear_interpolate(
+                        start.into(),
+                        end.into(),
+                        *amount)
+                    .into()
+            },
+
+            CubicRgb { start_slope, end_slope, amount } => {
+                Color::rgb_cubic_interpolate(
+                        start.into(),
+                        end.into(),
+                        *start_slope,
+                        *end_slope,
+                        *amount)
+                    .into()
+            },
+        }
+    }
+}
+
+impl Default for Interpolate {
+    fn default() -> Self {
+        Interpolate::LinearRgb { amount: 1.0 }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // Expr
 ////////////////////////////////////////////////////////////////////////////////
 /// Atma color expression for defining the behavior of a cell.
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 #[derive(Serialize, Deserialize)]
 pub enum Expr {
     /// An color expression with no color.
@@ -37,7 +97,9 @@ pub enum Expr {
     Color(Color),
 
     /// Performs an RGB multiply between the colors in the given cells.
-    RgbMultiply(CellRef<'static>, CellRef<'static>),
+    RgbMultiply(CellRef<'static>, CellRef<'static>, Interpolate),
+
+
 }
 
 impl Expr {
@@ -53,7 +115,7 @@ impl Expr {
             
             Expr::Color(c) => Ok(Some(c.clone())),
 
-            Expr::RgbMultiply(a, b) => match (
+            Expr::RgbMultiply(a, b, int) => match (
                 basic.cycle_detect_color(a, index_list)?,
                 basic.cycle_detect_color(b, index_list)?)
             {
@@ -61,7 +123,7 @@ impl Expr {
                     let [ra, ga, ba] = a.rgb_ratios();
                     let [rb, gb, bb] = b.rgb_ratios();
                     let rgb = Rgb::from([ra * rb, ga * gb, ba * bb]);
-                    Ok(Some(Color::from(rgb)))
+                    Ok(Some(int.apply(a, rgb)))
                 },
                 _ => Ok(None),
             },
